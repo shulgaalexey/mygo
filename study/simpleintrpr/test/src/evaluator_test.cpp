20905ea372562d144350e3630b8044fe7a01a5aa
shulga <a.shulga@samsung.com>
2017-12-18 11:41:56 +0900

    Added evaluation of different operators
    Signed-off-by: shulga <a.shulga@samsung.com>

--- study/simpleintrpr/test/src/evaluator_test.cpp
+++ study/simpleintrpr/test/src/evaluator_test.cpp
@@ -28,3 +28,33 @@ TEST_F(EvaluatorTest, shoudl_return_number_when_evaluate_list_with_number) {
 }
 
 // Pass [1 2 +], return 3
+TEST_F(EvaluatorTest, should_eval_expression_with_one_operator) {
+	double result = Evaluator::Evaluate({ _1, _2, plus });
+	EXPECT_EQ(3., result);
+}
+
+// Pass [1 2 *], return 2
+TEST_F(EvaluatorTest, should_eval_expression_with_one_multiplication) {
+	double result = Evaluator::Evaluate({ _1, _2, mul });
+	EXPECT_EQ(2., result);
+}
+
+// Pass [2 3 -], return -1
+TEST_F(EvaluatorTest, should_evaluate_with_one_subtraction) {
+	double result = Evaluator::Evaluate({ _2, _3, minus });
+	EXPECT_EQ( -1., result);
+}
+
+// Pass [5 2 /], return 2.5
+TEST_F(EvaluatorTest, should_evaluate_with_one_division) {
+	double result = Evaluator::Evaluate({ _5, _2, Interpreter::div });
+	EXPECT_EQ(2.5, result);
+}
+
+// (4 + 1) * 2 / (4 / (3 - 1)) = 4 1 + 2 * 4 3 1 - / / = 5
+TEST_F(EvaluatorTest, should_eval_complex_expression) {
+	double result = Evaluator::Evaluate({
+		_4, _1, plus, _2, mul, _4, _3, _1, minus,
+		Interpreter::div, Interpreter::div });
+	EXPECT_EQ( 5., result);
+}
