62929b8066c9fda22676a22f043727e9d5379fee
shulga <a.shulga@samsung.com>
2017-12-18 10:18:28 +0900

    Adding evaluator files
    Signed-off-by: shulga <a.shulga@samsung.com>

---
+++ study/simpleintrpr/src/evaluator.h
@@ -0,0 +1,10 @@
+#ifndef __EVALUATOR_H_INCLUDED__
+#define __EVALUATOR_H_INCLUDED__
+
+namespace Evaluator {
+
+
+
+} // namespace Evaluator
+
+#endif/*__EVALUATOR_H_INCLUDED__*/
---
+++ study/simpleintrpr/test/src/evaluator_test.cpp
@@ -0,0 +1,20 @@
+#include "gtest/gtest.h"
+#include "evaluator.h"
+#include "assert_range.h"
+#include <iostream>
+#include <string>
+
+using namespace Evaluator;
+
+class EvaluatorTest : public ::testing::Test {
+	protected:
+		virtual void SetUp() {
+		}
+
+		virtual void TeadDown() {
+		}
+};
+
+//
+TEST_F(EvaluatorTest, should_return) {
+}
