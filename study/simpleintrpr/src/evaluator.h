c4fd0bda0700ee5b87336d64000ddb9e06a0c1e0
shulga <a.shulga@samsung.com>
2017-12-18 13:45:05 +0900

    Refactored Evaluator: functionality is extracted to the dedicated class
    Signed-off-by: shulga <a.shulga@samsung.com>

--- study/simpleintrpr/src/evaluator.h
+++ study/simpleintrpr/src/evaluator.h
@@ -2,35 +2,90 @@
 #define __EVALUATOR_H_INCLUDED__
 
 #include "parser.h"
+#include <functional>
 
 namespace Evaluator {
 
-inline double Evaluate(const Tokens &tokens) {
-	std::vector<double> result{ 0. };
+namespace Detail {
 
-	auto pop = [&] () {
-		double d = result.back();
-		result.pop_back();
-		return d;
-	};
+class StackEvaluator {
+public:
+	StackEvaluator(const Tokens &tokens)
+		: m_current(tokens.cbegin())
+		, m_end(tokens.cend()) {}
 
-	for (const Token &token : tokens) {
-		if (token.Type() == TokenType::Number) {
-			result.push_back(token);
-		} else if (token == Token(Operator::Plus)) {
-			result.push_back(pop() + pop());
-		} else if (token == Token(Operator::Minus)) {
-			double d = pop();
-			result.push_back(pop() - d);
-		} else if (token == Token(Operator::Mul)) {
-			result.push_back(pop() * pop());
-		} else if (token == Token(Operator::Div)) {
-			double d = pop();
-			result.push_back(pop() / d);
+	void Evaluate() {
+		for (; m_current != m_end; ++m_current) {
+			EvaluateCurrentToken();
 		}
 	}
-	return pop();
+
+	double Result() const { return m_stack.empty() ? 0 : m_stack.back(); }
+
+private:
+	void EvaluateCurrentToken() {
+		switch (m_current->Type()) {
+		case TokenType::Operator:
+			EvaluateOperator();
+			break;
+		case TokenType::Number:
+			EvaluateNumber();
+			break;
+		default:
+			throw std::out_of_range("TokenType");
+		}
+	}
+
+	void EvaluateOperator() {
+		double second = PopOperand();
+		double first = PopOperand();
+		m_stack.push_back(BinaryFunctionFor(*m_current)(first, second));
+	}
+
+	void EvaluateNumber() {
+		m_stack.push_back(*m_current);
+	}
+
+	double PopOperand() {
+		double back = m_stack.back();
+		m_stack.pop_back();
+		return back;
+	}
+
+	static const std::function<double(double, double)> &BinaryFunctionFor(
+			Operator op) {
+		static const std::map<Operator,
+		     std::function<double(double, double)>> functions {
+			     {Operator::Plus, std::plus<double>() },
+			     {Operator::Minus, std::minus<double>() },
+			     {Operator::Mul, std::multiplies<double>() },
+			     {Operator::Div, std::divides<double>() }
+		     };
+
+		auto found = functions.find(op);
+		if (found == functions.cend()) {
+			throw std::logic_error("Operator not found");
+		}
+		return found->second;
+	}
+
+private:
+	Tokens::const_iterator m_current;
+	Tokens::const_iterator m_end;
+	std::vector<double> m_stack;
+};
+
+
+
+
+} // namespace Detail
+
+inline double Evaluate(const Tokens &tokens) {
+	Detail::StackEvaluator evaluator(tokens);
+	evaluator.Evaluate();
+	return evaluator.Result();
 }
+
 
 } // namespace Evaluator
 
